b2df2caad6e8492d49e1775d79f67b0947c58ddb
takashi imagire <imagire@gmail.com>
2020-05-28 19:09:47 +0900

    add files

---
+++ src/great_code/main/main.cpp
@@ -0,0 +1,9 @@
+﻿char mojiretsu[1000];
+int mojisuu=0;
+#include <stdio.h>
+#include <stdlib.h>
+#pragma warning(disable : 4996)
+#ifdef this_is_a_great_codo
+問題：1から順番に数字を発言するのだけれども、その数字が3で割り切れれはFizzと発言して、5ならBuzz、１５でならFizzBuzzと発言する
+#endif
+int main(){while(mojisuu++<1000)sprintf(mojiretsu,"%d",mojisuu),printf("%s, ",mojisuu%15?mojisuu%3?mojisuu%5?mojiretsu:"Buzz":"Fizz":"FizzBuzz");system("PAUSE");}
