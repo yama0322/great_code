7fd4b4061ccb19f64895b6edf9bd24d23c53f161
yama0322 <55902857+yama0322@users.noreply.github.com>
2020-06-08 15:40:15 +0900

    Update mian.cpp

--- src/mian.cpp
+++ src/mian.cpp
@@ -6,4 +6,10 @@ int mojisuu = 0;
 #ifdef this_is_a_great_codo
 問題：1から順番に数字を発言するのだけれども、その数字が3で割り切れれはFizzと発言して、5ならBuzz、１５でならFizzBuzzと発言する
 #endif
-int main() { while (mojisuu++ < 1000)sprintf(mojiretsu, "%d", mojisuu), printf("%s, ", mojisuu % 15 ? mojisuu % 3 ? mojisuu % 5 ? mojiretsu : "Buzz" : "Fizz" : "FizzBuzz"); system("PAUSE"); }
+int main() 
+{ 
+	while (mojisuu++ < 1000)
+		sprintf(mojiretsu, "%d", mojisuu), 
+		printf("%s, ", mojisuu % 15 ? mojisuu % 3 ? mojisuu % 5 ? mojiretsu : "Buzz" : "Fizz" : "FizzBuzz"); 
+	system("PAUSE"); 
+}
